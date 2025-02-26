cd9577b8625fa593bbfedc8cded7e3c162df07aa
DRSarcenoR <diego.sarceno@chn.com.gt>
2025-02-26 11:56:55 -0600

    v1.7.2

--- CHN/CHN/general.py
+++ CHN/CHN/general.py
@@ -35,6 +35,9 @@ import seaborn as sns
 # ML
 from sklearn.cluster import KMeans
 
+# text
+from text import text_management
+
 # apoyo
 from datetime import datetime, timedelta
 import time
@@ -388,6 +391,171 @@ class Other:
         except Exception as e:
             print(f'Error: {e}')
             return False # GG
+    
+    def infoClientes(clientes : str | tuple, output : str | None = None) -> tuple | None:
+        # creamos el objeto
+        conn = Connections()
+        tm = text_management()
+        
+        # query en forma lambda
+        query = lambda clientes: f'''with info_clientes as (
+        select
+            cli.cliente_Skey,
+            cli.cod_cliente,
+            cli.nombre,
+            convert(varchar, cli.fecha_alta, 23) fecha_alta,
+            cli.clase,
+            cli.pais,
+            cli.profesion,
+            cli.direccion_cliente,
+            cli.estadoCliente,
+            cli.fecha_ultima_actualizacion fecha_ultima_actualizacion,
+            cli.agencia_ultima_actualizacion,
+            cli.cliente_pep,
+            cli.cliente_cpe,
+            cli.tipoPersona,
+            cli.tipo_sociedad,
+            cli.sectorEconomico,
+            cli.actividad_economica,
+            cli.sector_economico_ive,
+            cli.institucion,
+            cli.puesto,
+            cli.codigo_empleado,
+            cli.direcciones_np,
+            cli.nombre_notario,
+            cli.cod_representante_legal,
+            cli.nombre_representante_legal
+        from dim_cliente cli
+        where cli.cod_cliente in {clientes}
+    ),
+    prods_creds as (
+        select 
+            ic.cliente_Skey,
+            ic.cod_cliente,
+            ic.nombre,
+            col.cod_col_cartera as cod_prod,
+            ce.nombre as estado,
+            prod.descripcion as producto,
+            sp.descripcion as subproducto,
+            suc.cod_sucursal,
+            suc.nombre as sucursal,
+            origen = 'COLOCACION (Credito)'
+        from fac_colocacion col
+        right join info_clientes ic on ic.cliente_Skey = col.cliente_Skey
+            --and (col.col_estado_Skey between 25 and 36 or col.col_estado_Skey between 45 and 48)
+        join dim_producto prod on prod.producto_SKey = col.producto_SKey
+        join dim_subproducto sp on sp.subproducto_SKey = col.subproducto_SKey
+        join dim_sucursal suc on suc.sucursal_Skey = col.sucursal_Skey
+        join dim_col_estado ce on ce.col_estado_Skey = col.col_estado_Skey
+        --where (col.col_estado_Skey between 25 and 36 or col.col_estado_Skey between 45 and 48) -- no cancelados o anulados
+    ),
+    prods_cuentas as (
+        select 
+            ic.cliente_Skey,
+            ic.cod_cliente,
+            ic.nombre,
+            cap.cod_cuenta as cod_prod,
+            ec.nombre as estado,
+            cp.descripcion as producto,
+            cs.descripcion as subproducto,
+            suc.cod_sucursal,
+            suc.nombre as sucursal,
+            origen = 'CAPTACION (Cuenta)'
+        from fac_captacion cap
+        right join info_clientes ic on ic.cliente_Skey = cap.cliente_Skey
+            --and cap.estado_cartera_Skey = 2 -- productos activos
+        join dim_cap_producto cp on cp.producto_SKey = cap.producto_SKey
+        join dim_cap_subproducto cs on cs.subproducto_SKey = cap.subproducto_SKey
+        join dim_sucursal suc on suc.sucursal_Skey = cap.sucursal_apertura_skey
+        join dim_estado_cartera ec on ec.estado_cartera_SKey = cap.estado_cartera_Skey
+        --where cap.estado_cartera_Skey = 2 -- productos activos
+    ),
+    prods_trj as (
+        select 
+            ic.cliente_Skey,
+            ic.cod_cliente,
+            ic.nombre,
+            trj.cod_col_cartera as cod_prod,
+            ec.nombre as estado,
+            producto = 'TARJETA CREDITO',
+            tt.nombre as subproducto,
+            suc.cod_sucursal,
+            suc.nombre as sucursal,
+            origen = 'TARJETA (T.C.)'
+        from fac_tarjeta as trj
+        right join info_clientes ic on ic.cliente_Skey = trj.cliente_Skey
+            --and trj.estado = 2 -- activa
+        join dim_tipotarjeta tt on tt.tipotarjeta_Skey = trj.tipotarjeta_skey
+        join dim_sucursal suc on suc.sucursal_Skey = trj.sucursal_skey
+        join dim_estado_cartera ec on ec.estado_cartera_SKey = trj.estado
+        --where trj.estado = 2 -- activa
+    ),
+    productos as (
+        select * from prods_creds
+        union 
+        select * from prods_cuentas
+        union 
+        select * from prods_trj
+    ),
+    accionistas as (
+        select
+            ic.cliente_Skey,
+            ic.cod_cliente,
+            ic.nombre,
+            acc.identificacion_accionista,
+            acc.nombre_accionista,
+            acc.porcentaje_accionista
+        from fac_accionista acc
+        join info_clientes ic on ic.cliente_Skey = acc.cliente_skey
+    ),
+    beneficiarios as (
+        select
+            pc.cliente_Skey,
+            pc.cod_cliente,
+            pc.nombre,
+            pc.cod_prod,
+            pc.estado,
+            pc.producto,
+            pc.subproducto,
+            pc.cod_sucursal,
+            pc.sucursal,
+            ben.nombre_beneficiario,
+            ben.porcentaje_beneficiario
+        from fac_beneficiario ben
+        join prods_cuentas pc on pc.cod_prod = ben.cod_cuenta
+    )'''
+        
+        # comprobamos que sea alguno de los dos tipos de dato que nos interesa
+        if isinstance(clientes, str):
+            clientes = tuple(clientes)
+        elif not isinstance(clientes, tuple):
+            return None
+        
+        # solicitamos la informacion
+        infoClienteQ = 'select * from info_clientes'
+        productosQ = 'select * from productos'
+        accionistasQ = 'select * from accionistas'
+        beneficiariosQ = 'select * from beneficiarios'
+        infoCliente = conn.connection(query(clientes) + infoClienteQ, 1)
+        productos = conn.connection(query(clientes) + productosQ, 1)
+        accionistas = conn.connection(query(clientes) + accionistasQ, 1)
+        beneficiarios = conn.connection(query(clientes) + beneficiariosQ, 1)
+
+        # limpiamos los caracteres no imprimibles
+        infoCliente = tm.no_imprimibles(infoCliente)
+        productos = tm.no_imprimibles(productos)
+        accionistas = tm.no_imprimibles(accionistas)
+        beneficiarios = tm.no_imprimibles(beneficiarios)
+
+        # en caso que se desee se exporta en un archivo excel
+        if output:
+            with pd.ExcelWriter(output, engine='openpyxl') as op:
+                infoCliente.to_excel(op, sheet_name='INFOCLIENTE', index=False)
+                productos.to_excel(op, sheet_name='PRODUCTOS', index=False)
+                accionistas.to_excel(op, sheet_name='ACCIONISTAS', index=False)
+                beneficiarios.to_excel(op, sheet_name='BENEFICIARIOS', index=False)
+        
+        return infoCliente, productos, accionistas, beneficiarios
 
 
 
