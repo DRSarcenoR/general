67814429cd8ff400a47c9405f327a3730b88fba5
DRSarcenoR <diego.sarceno@chn.com.gt>
2025-05-29 16:25:52 -0600

    v3.2.1

--- CHN/setup.py
+++ CHN/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='CHN',
-    version='3.1.4',
+    version='3.2.1',
     description='Colección de métodos y funciones útiles para análisis y ciencia de datos, web-scrapping y tareas comunes.',
     author='Diego Sarceño',
     author_email='dsarceno68@gmail.com',
